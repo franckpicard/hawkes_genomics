7ac73a18f94c192fa262cd4febcaf91ea815ddce
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2024-01-09 11:15:25 +0100

    fix typo in compute_hawkes_histrogram

--- R/compute_hawkes_histogram.R
+++ R/compute_hawkes_histogram.R
@@ -23,7 +23,7 @@ compute_hawkes_histogram <- function(
   cmd <- c(cmd, paste("-kernel", kernel))
   cmd <- c(cmd, "-lambda 1")
   system2(
-    file.path(find.package(package = "hawkes.genomics"), "bin", "hawkes_bin"),
+    file.path(find.package(package = "hawkesGenomics"), "bin", "hawkes_bin"),
     cmd,
     stdout = TRUE
   ) |>
